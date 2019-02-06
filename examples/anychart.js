5c2fbd467b2a6e93f65b6e8add9cfa2a73a4b32d
chernetsky <chernetsky@gmail.com>
2019-02-06 13:30:53 +0800

    quotes

--- examples/anychart.js
+++ examples/anychart.js
@@ -2,63 +2,64 @@
   var acGlobal;
 
   freeboard.loadWidgetPlugin({
-    "type_name": "anychart_freeboard_plugin",
+    'type_name': 'anychart_freeboard_plugin',
 
-    "display_name": "Anychart",
+    'display_name': 'Anychart',
 
-    "description": "AnyChart -- <strong>best charting solutions!</strong>",
+    'description': 'AnyChart -- <strong>best charting solutions!</strong>',
 
-    "external_scripts": [
-      "https://cdn.anychart.com/releases/8.5.0/js/anychart-bundle.min.js"
+    'external_scripts': [
+      'https://cdn.anychart.com/releases/8.5.0/js/anychart-bundle.min.js'
     ],
 
-    "fill_size": true,
-    "settings": [
+    'fill_size': true,
+    'settings': [
       {
-        "name": "data_source",
-        "display_name": "Data source",
-        "type": "calculated",
-        "multi_input": true
+        'name': 'data_source',
+        'display_name': 'Data source',
+        'type': 'calculated',
+        'multi_input': true,
+        required: true
       },
       {
-        "name": "type",
-        "display_name": "Chart type",
-        "type": "option",
-        "options": [
+        'name': 'type',
+        'display_name': 'Chart type',
+        'type': 'option',
+        'options': [
           {
-            "name": "line",
-            "value": "line"
+            'name': 'line',
+            'value': 'line'
           },
           {
-            "name": "column",
-            "value": "column"
+            'name': 'column',
+            'value': 'column'
           },
           {
-            "name": "pie",
-            "value": "pie"
+            'name': 'pie',
+            'value': 'pie'
           }
         ]
       },
       {
-        "name": "size",
-        "display_name": "Size",
-        "type": "option",
-        "options": [
+        'name': 'size',
+        'display_name': 'Size',
+        'type': 'option',
+        'options': [
           {
-            "name": "2",
-            "value": 2
+            'name': '2',
+            'value': 2
           },
           {
-            "name": "3",
-            "value": 3
+            'name': '3',
+            'value': 3
           },
           {
-            "name": "4",
-            "value": 4
+            'name': '4',
+            'value': 4
           },
           {
-            "name": "5",
-            "value": 5
+            'name': '5',
+            'value': 5
           }
         ]
       }
@@ -99,7 +100,7 @@
       currentSettings = newSettings;
 
       if (newSettings.type !== oldType && chart) {
-        console.log("chart.dispose() 1");
+        console.log('chart.dispose() 1');
         chart.dispose();
         chart = null;
         self.render(container);
@@ -107,17 +108,21 @@
     };
 
     self.onCalculatedValueChanged = function(settingName, newValue) {
-      // Remember we defined "the_text" up above in our settings.
-      console.log(settingName, "changed");
-      if (settingName === "data_source") {
-        console.log(newValue);
-        dataSet.append(newValue)
+      // Remember we defined 'the_text' up above in our settings.
+      console.log(settingName, 'changed');
+      console.log(newValue);
+      switch (settingName) {
+        case 'data_source':
+          dataSet.append(newValue);
+          break;
+        case 'mapping':
+          break;
       }
     };
 
     self.onDispose = function() {
       if (chart) {
-        console.log("chart.dispose() 2");
+        console.log('chart.dispose() 2');
         chart.dispose();
       }
     };
