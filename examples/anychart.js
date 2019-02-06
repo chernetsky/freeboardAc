6e72072a8867f06a7c72d3f8e8e5ecd68be4c78b
chernetsky <chernetsky@gmail.com>
2019-02-06 18:37:12 +0800

    ce opening

--- examples/anychart.js
+++ examples/anychart.js
@@ -2,66 +2,73 @@
   var acGlobal;
 
   freeboard.loadWidgetPlugin({
-    'type_name': 'anychart_freeboard_plugin',
-
-    'display_name': 'Anychart',
-
-    'description': 'AnyChart -- <strong>best charting solutions!</strong>',
-
-    'external_scripts': [
-      'https://cdn.anychart.com/releases/8.5.0/js/anychart-bundle.min.js'
+    type_name: 'anychart_freeboard_plugin',
+    display_name: 'Anychart',
+    description: 'AnyChart -- <strong>best charting solutions!</strong>',
+    external_scripts: [
+      'https://cdn.anychart.com/releases/8.5.0/js/anychart-bundle.min.js',
+      'plugins/thirdparty/anychart-editor.min.js',
+      'plugins/thirdparty/anychart-editor.min.css'
     ],
 
-    'fill_size': true,
-    'settings': [
+    fill_size: true,
+    settings: [
       {
-        'name': 'data_source',
-        'display_name': 'Data source',
-        'type': 'calculated',
-        'multi_input': true,
-        required: true
+        name: 'data_source',
+        display_name: 'Data source',
+        type: 'calculated',
+        multi_input: true,
+        required: true,
+        default_value: [[1, 0, 3, 6, 10]]
       },
       {
-        'name': 'type',
-        'display_name': 'Chart type',
-        'type': 'option',
-        'options': [
+        name: 'type',
+        display_name: 'Chart type',
+        type: 'option',
+        options: [
           {
-            'name': 'line',
-            'value': 'line'
+            name: 'line',
+            value: 'line'
           },
           {
-            'name': 'column',
-            'value': 'column'
+            name: 'column',
+            value: 'column'
           },
           {
-            'name': 'pie',
-            'value': 'pie'
+            name: 'pie',
+            value: 'pie'
           }
         ]
       },
       {
-        'name': 'size',
-        'display_name': 'Size',
-        'type': 'option',
-        'options': [
+        name: 'size',
+        display_name: 'Size',
+        type: 'option',
+        options: [
           {
-            'name': '2',
-            'value': 2
+            name: '2',
+            value: 2
           },
           {
-            'name': '3',
-            'value': 3
+            name: '3',
+            value: 3
           },
           {
-            'name': '4',
-            'value': 4
+            name: '4',
+            value: 4
           },
           {
-            'name': '5',
-            'value': 5
+            name: '5',
+            value: 5
           }
         ]
+      },
+      {
+        name: 'run_editor',
+        display_name: 'Run editor',
+        type: 'boolean',
+        default_value: true,
+        description: 'Run chart editor after close this dialog'
       }
     ],
 
@@ -75,19 +82,95 @@
   var anychartWidget = function(settings) {
     var self = this;
     var container;
+    var editor;
     var chart;
+    var code;
     var dataSet = acGlobal.data.set();
-
     var currentSettings = settings;
+    var editorComplete = false;
+    // var editorWrapper = $('<div id="editor-wrapper" style="z-index: 1000;"></div>');
 
-    self.render = function(containerElement) {
-      if (!chart) {
-        container = containerElement;
-        chart = acGlobal[currentSettings.type](dataSet);
-        chart.container(containerElement);
-        chart.draw();
+    self.runEditor = function(){
+      if (!editor) {
+        editor = new acGlobal['chartEditor'];
+        editor.step('data', false);
+        editor.step('export', false);
+        editor.data([
+          {'x': 'Hello', 'value': 10},
+          {'x': 'Freeboard', 'value': 20},
+          {'x': 'World', 'value': 15}
+        ]);
+        editor.dialogRender();
+
+        editor.listenOnce('editorComplete', function() {
+          editorComplete = true;
+
+          // Get from editor javascript code that creates configured chart
+          code = editor['getJavascript']({
+            'minify': true,
+            'addData': false,
+            'addMarkers': true,
+            'wrapper': '',
+            'container': ''
+          });
+
+          // Save code and close editor dialog
+          self.closeEditor(code);
+        });
+
+        editor.listen('close', function(evt) {
+          if (!editorComplete && evt.target === editor)
+            self.closeEditor(null);
+        });
       }
-      console.log('render() call', currentSettings.type);
+
+      editorComplete = false;
+      editor.dialogVisible(true);
+    };
+
+    self.closeEditor = function(opt_code){
+      if (opt_code)
+        self.render(container);
+    };
+
+    self.render = function(element) {
+      if (!code) {
+        container = element;
+        self.runEditor();
+      } else {
+        self.drawChart();
+      }
+    };
+
+    self.drawChart = function () {
+      if (chart) {
+        chart.dispose();
+      }
+
+      var codeSplit = code.split(/\/\*=rawData.+rawData=\*\//);
+      if (codeSplit.length === 2) {
+        // Chart creation code part
+        var code1 = '(function(){' + codeSplit[0] + 'return chart;})();';
+
+        // Data apply and chart settings code part
+        var code2 = '(function(){ return function(chart, rawData){' + codeSplit[1] + '}})();';
+        console.log(code1, code2);
+
+        // Create chart instance
+        chart = eval(code1);
+
+        if (!chart) return null;
+
+        // Invoke second part of code: pass data and apply chart appearance settings
+        var code2func = eval(code2);
+        code2func.apply(null, [chart, dataSet.data()]);
+
+        // todo: DEBUG
+        // chart.data(dataSet);
+
+        chart['container'](container);
+        chart['draw']();
+      }
     };
 
     self.getHeight = function() {
@@ -96,21 +179,19 @@
     };
 
     self.onSettingsChanged = function(newSettings) {
-      var oldType = currentSettings.type;
+      console.log("onSettingsChanged", newSettings);
       currentSettings = newSettings;
 
-      if (newSettings.type !== oldType && chart) {
-        console.log('chart.dispose() 1');
-        chart.dispose();
-        chart = null;
+      if (currentSettings.run_editor) {
+        self.runEditor();
+        currentSettings.run_editor = false;
+        dataSet.append(currentSettings.data_source);
+      } else {
         self.render(container);
       }
     };
 
     self.onCalculatedValueChanged = function(settingName, newValue) {
-      // Remember we defined 'the_text' up above in our settings.
-      console.log(settingName, 'changed');
-      console.log(newValue);
       switch (settingName) {
         case 'data_source':
           dataSet.append(newValue);
@@ -122,8 +203,8 @@
 
     self.onDispose = function() {
       if (chart) {
-        console.log('chart.dispose() 2');
         chart.dispose();
+        dataSet.dispose();
       }
     };
   }
